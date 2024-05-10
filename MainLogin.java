1e8a438526c17b0c74f5478d4a89b44440bc4f61
valeriana <claudiaae2@gmail.com>
2024-05-09 22:47:00 -0600

    version2

--- MainLogin.java
+++ MainLogin.java
@@ -1,33 +1,35 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MainLogin {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         String USUARIO_CORRECTO = "claudia";
         String CONTRASEÑA_CORRECTA = "030405";
 
-        
         Scanner reader = new Scanner(System.in);
 
-        
         System.out.println("Ingrese su nombre de usuario:");
         String usuario = reader.nextLine();
         System.out.println("Ingrese su contraseña:");
         String contraseña = reader.nextLine();
 
-        
-        while (!usuario.equals(USUARIO_CORRECTO) || !contraseña.equals(CONTRASEÑA_CORRECTA)) {
-            System.out.println("Nombre de usuario o contraseña incorrectos. Inténtelo de nuevo.");
+        boolean inicioSesionExitoso = false; // Inicializamos la variable
 
-            
-            System.out.println("Ingrese su nombre de usuario:");
-            usuario = reader.nextLine();
-            System.out.println("Ingrese su contraseña:");
-            contraseña = reader.nextLine();
+        while (!inicioSesionExitoso) {
+
+            if (usuario.equals(USUARIO_CORRECTO) && contraseña.equals(CONTRASEÑA_CORRECTA)) {
+                inicioSesionExitoso = true; // Marcamos el inicio de sesión como exitoso
+                System.out.println("Inicio de sesión exitoso. ¡Bienvenido a la tienda, " + usuario + "!");
+            } else {
+                System.out.println("Nombre de usuario o contraseña incorrectos. Inténtelo de nuevo.");
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("Ingrese su nombre de usuario:");
+                usuario = reader.nextLine();
+                System.out.println("Ingrese su contraseña:");
+                contraseña = reader.nextLine();
+            }
         }
-
-        
-        System.out.println("Inicio de sesión exitoso. ¡Bienvenido a la tienda, " + usuario + "!");
     }
 }
